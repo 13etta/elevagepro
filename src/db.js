a07053fc6b4ef4020adca4d2d0ad35b46731fa1c
13etta <loic.o.darras@gmail.com>
2026-04-24 19:35:22 +0000

    Lot 2

--- src/db.js
+++ src/db.js
@@ -1,12 +1,4 @@
 const { Pool } = require('pg');
-
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error('DATABASE_URL is required');
-}
-
-const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
@@ -19,13 +11,4 @@ const pool = new Pool({
 module.exports = {
   query: (text, params) => pool.query(text, params),
   pool
-};
-
-async function query(text, params = []) {
-  return pool.query(text, params);
-}
-
-module.exports = {
-  pool,
-  query,
 };
