ded4e5dddfbfd57d308d1554ca6f04c7cea38d80
13etta <loic.o.darras@gmail.com>
2026-04-24 19:13:41 +0000

    Lot 2

--- src/db.js
+++ src/db.js
@@ -6,12 +6,20 @@ if (!connectionString) {
   throw new Error('DATABASE_URL is required');
 }
 
+const { Pool } = require('pg');
+require('dotenv').config();
+
 const pool = new Pool({
-  connectionString,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: process.env.DATABASE_URL,
+  ssl: { 
+    rejectUnauthorized: false 
+  }
 });
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  pool
+};
 
 async function query(text, params = []) {
   return pool.query(text, params);
