131044f3b2cae263069dbbd3d34446985f29c2f2
13etta <loic.o.darras@gmail.com>
2026-04-26 05:42:07 +0000

    Debogage

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const PgSession = connectPgSimple(session);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(
