86df21fd45f56804f5ac1b7b6b7750e4b2c7b50d
13etta <loic.o.darras@gmail.com>
2026-04-27 08:36:14 +0000

    Amelioration Dog

--- src/routes/heats.routes.js
+++ src/routes/heats.routes.js
@@ -1,28 +1,23 @@
 const express = require('express');
-const { requireAuth } = require('../middleware/auth');
-
-const router = express.Router();
-
-router.get('/', requireAuth, (req, res) => {
-  res.status(501).render('coming-soon', {
-    title: 'Module en préparation',
-    user: req.session.user,
-    moduleName: req.baseUrl.replace('/', ''),
-  });
-});
-const express = require('express');
 const router = express.Router();
 const heatsController = require('../controllers/heats.controller');
 const { requireAuth } = require('../middleware/auth');
 
+// Protection globale du module
 router.use(requireAuth);
 
+// Lecture
 router.get('/', heatsController.listHeats);
+
+// Création
 router.get('/new', heatsController.getCreateForm);
 router.post('/new', heatsController.createHeat);
+
+// Édition
 router.get('/:id/edit', heatsController.getEditForm);
 router.post('/:id/edit', heatsController.updateHeat);
+
+// Suppression
 router.post('/:id/delete', heatsController.deleteHeat);
 
-module.exports = router;
 module.exports = router;
