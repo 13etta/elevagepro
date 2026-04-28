59c36ad15435a704454239b1a082fc7d9855cd1d
13etta <loic.o.darras@gmail.com>
2026-04-28 14:07:08 +0200

    Support legacy puppy edit and delete route formats

--- src/routes/puppies.routes.js
+++ src/routes/puppies.routes.js
@@ -6,10 +6,20 @@ const { requireAuth } = require('../middleware/auth');
 router.use(requireAuth);
 
 router.get('/', puppiesController.listPuppies);
+
 router.get('/new', puppiesController.getForm);
+router.post('/new', puppiesController.savePuppy);
 router.post('/save', puppiesController.savePuppy);
+
+// Format utilisé par la nouvelle liste : /puppies/edit/:id
 router.get('/edit/:id', puppiesController.getForm);
 router.post('/save/:id', puppiesController.savePuppy);
 router.get('/delete/:id', puppiesController.deletePuppy);
+
+// Format historique déjà utilisé par certaines vues : /puppies/:id/edit
+router.get('/:id/edit', puppiesController.getForm);
+router.post('/:id/edit', puppiesController.savePuppy);
+router.get('/:id/delete', puppiesController.deletePuppy);
+router.post('/:id/delete', puppiesController.deletePuppy);
 
 module.exports = router;
