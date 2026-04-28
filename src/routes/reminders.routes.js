bffd0a508d57e81250a46eaa88b91022c28b3e30
13etta <loic.o.darras@gmail.com>
2026-04-28 15:24:44 +0200

    Wire reminders routes to full controller

--- src/routes/reminders.routes.js
+++ src/routes/reminders.routes.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const { requireAuth } = require('../middleware/auth');
+const remindersController = require('../controllers/reminders.controller');
 
 const router = express.Router();
 
-router.get('/', requireAuth, (req, res) => {
-  res.status(501).render('coming-soon', {
-    title: 'Module en préparation',
-    user: req.session.user,
-    moduleName: req.baseUrl.replace('/', ''),
-  });
-});
+router.use(requireAuth);
+
+router.get('/', remindersController.listReminders);
+router.get('/new', remindersController.getReminderForm);
+router.post('/new', remindersController.saveReminder);
+router.get('/:id/edit', remindersController.getReminderForm);
+router.post('/:id/edit', remindersController.saveReminder);
+router.get('/:id/complete', remindersController.completeReminder);
+router.post('/:id/complete', remindersController.completeReminder);
+router.get('/:id/reopen', remindersController.reopenReminder);
+router.post('/:id/reopen', remindersController.reopenReminder);
+router.get('/:id/delete', remindersController.deleteReminder);
+router.post('/:id/delete', remindersController.deleteReminder);
 
 module.exports = router;
