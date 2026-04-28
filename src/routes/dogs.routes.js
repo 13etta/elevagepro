ea8df4124ed1861933cb35bd636b2fdc3848aaad
13etta <loic.o.darras@gmail.com>
2026-04-28 15:59:17 +0200

    Add dog detail route

--- src/routes/dogs.routes.js
+++ src/routes/dogs.routes.js
@@ -3,21 +3,18 @@ const router = express.Router();
 const dogsController = require('../controllers/dogs.controller');
 const { requireAuth } = require('../middleware/auth');
 
-// Protection des routes
 router.use(requireAuth);
 
-// Lecture
 router.get('/', dogsController.listDogs);
 
-// Création
 router.get('/new', dogsController.getCreateForm);
 router.post('/new', dogsController.createDog);
 
-// Modification
+router.get('/:id', dogsController.showDog);
+
 router.get('/:id/edit', dogsController.getEditForm);
 router.post('/:id/edit', dogsController.updateDog);
 
-// Suppression
 router.post('/:id/delete', dogsController.deleteDog);
 
 module.exports = router;
