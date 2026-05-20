90823e2ac5e11dde59e3e08307ac0358671dd1b6
13etta <loic.o.darras@gmail.com>
2026-05-20 13:55:04 +0200

    Ajouter migration structure assignations

--- sql/migrate.js
+++ sql/migrate.js
@@ -44,6 +44,7 @@ async function runMigrations() {
     '013_registry_backfill_existing_dogs.sql',
     '014_registry_litter_events.sql',
     '015_qa_hardening.sql',
+    '016_structure_assignments.sql',
   ];
 
   console.log('Démarrage des migrations...');
