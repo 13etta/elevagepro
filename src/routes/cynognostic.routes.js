6b412bc7fb5f9d4cb20e4af3de0bf27c68694360
13etta <loic.o.darras@gmail.com>
2026-05-25 07:45:12 +0200

    Ajouter routes StatGescon

--- src/routes/cynognostic.routes.js
+++ src/routes/cynognostic.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const cynognosticController = require('../controllers/cynognostic.controller');
+const statgesconController = require('../controllers/statgescon.controller');
 const { requireAuth } = require('../middleware/auth');
 const { verifyCsrf } = require('../middleware/csrf');
 
@@ -10,5 +11,7 @@ router.get('/', cynognosticController.index);
 router.post('/analyze', verifyCsrf, cynognosticController.runAnalysis);
 router.get('/reports/:id', cynognosticController.showReport);
 router.post('/watch-profiles', verifyCsrf, cynognosticController.createWatch);
+router.get('/statgescon', statgesconController.form);
+router.post('/statgescon/search', verifyCsrf, statgesconController.search);
 
 module.exports = router;
