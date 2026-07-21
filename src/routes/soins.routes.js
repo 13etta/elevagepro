30655f63678ebdea80e03150642d03d90f2f1428
13etta <loic.o.darras@gmail.com>
2026-07-21 09:25:05 +0200

    feat: expose health register PDF route

--- src/routes/soins.routes.js
+++ src/routes/soins.routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { requireAuth } = require('../middleware/auth');
 const soinsController = require('../controllers/soins.controller');
+const soinsPdfController = require('../controllers/soins-pdf.controller');
 
 router.use(requireAuth);
 
 router.get('/', soinsController.listSoins);
+router.get('/registre-sanitaire.pdf', soinsPdfController.exportHealthRegister);
 router.post('/new', soinsController.createSoin);
 router.get('/:id/edit', soinsController.editSoin);
 router.post('/:id/edit', soinsController.updateSoin);
